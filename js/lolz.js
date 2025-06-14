06a894d00f6e9907ac0c27037e865f68f25af3b8
Anna Powaska <annapowaska@op.pl>
2025-06-14 11:26:58 +0200

    n

--- js/lolz.js
+++ js/lolz.js
@@ -76,8 +76,8 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener("DOMContentLoaded", function() {
     let playlist = [
         "muza/a.mp3",
-        "muza/b.mp3",
-        "muza/c.mp3"
+        "muza/c.mp3",
+        "muza/b.mp3"
     ]; // Dodaj więcej linków, jeśli chcesz!
 
     let currentTrack = 0;
