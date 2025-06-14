4bbc28ec2908686f0c7b5dc90d83da3219b584cb
Anna Powaska <annapowaska@op.pl>
2025-06-14 10:35:59 +0200

    s

--- js/xd.js
+++ js/xd.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let audio = new Audio("a.mp3"); // Podmień na link do Twojej muzyki!
+    let audio = new Audio("muza/a.mp3"); // Poprawiona ścieżka do MP3!
     audio.volume = 0.5; // Możesz zmienić głośność (0.0 - 1.0)
-    audio.play().catch(error => console.log("Auto-play blocked, user interaction required."));
+    
+    audio.play().catch(error => {
+        console.log("Auto-play blocked, user interaction required.");
+        document.addEventListener("click", function() {
+            audio.play();
+        }, { once: true });
+    });
 });
