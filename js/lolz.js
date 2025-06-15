28ec01596b916968af9e69cf6ed3e84b4ce368b1
Anna Powaska <annapowaska@op.pl>
2025-06-15 06:40:21 +0200

    s

--- js/lolz.js
+++ js/lolz.js
@@ -99,10 +99,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.body.classList.add("party-mode"); // aktywuje tło
 document.getElementById("party-screen").style.display = "block"; // pokazuje rave'owy ekran
+
+
 const favicons = [
-  "disco1.svg",
-  "disco2.svg",
-  "disco3.svg"
+  "js/disco1.svg",
+  "js/disco2.svg",
+  "js/disco3.svg"
 ];
 
 let i = 0;
@@ -113,4 +115,4 @@ setInterval(() => {
     link.href = favicons[i % favicons.length];
     i++;
   }
-}, 1200); // zmienia co 1.2 sekundy
+}, 1200); // zmiana co 1.2 sekundy
